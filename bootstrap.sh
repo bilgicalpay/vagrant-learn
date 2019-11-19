7ee8e90b7f30fc5c55ca6632471382b551d13942
Alpay Bilgiç <bilgicalpay@users.noreply.github.com>
2019-11-19 14:46:03 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,15 @@ sudo pip install virtualenv
 #sudo pip install --upgrade pip bu başa bela 
 # düzeltmek için pip install pip==1.5.6  python 2.7 için 1.5.6
 sudo pip install pip==1.5.6
-sudo apt-get build-dep python-mysqldb -y
-sudo pip install MySQL-python -y
+sudo apt install python-mysqldb -q -y
 sudo pip install gunicorn
-sudo apt  install nginx -q -y 
-sudo pip install Flask -y
+sudo apt install nginx -q -y 
+sudo pip install Flask 
+sudo apt-get install libmysqlclient-dev -q -y
+sudo pip install MySQL-python 
+sudo pip install mysql-connector-python
 ## amac: flask(web app)-->gunicorn(WSGI server):port-->nginx(web server):port
-cd /var/www
+cd /var/www/codebase
 sudo virtualenv --no-site-packages ..
 sudo mkdir /var/www/codebase
 sudo cp /vagrant/* /var/www/codebase
@@ -24,7 +26,7 @@ git clone https://gist.github.com/vedit/32f37e0b8f842521fbc89e9d8e2bfaf9
 sudo cp /var/www/codebase/32f37e0b8f842521fbc89e9d8e2bfaf9/application.py ../
 ## asagidaki çalıştı
 python application.py
-# ama gunicorn istemişler onda kod hata veriyore gunicorn ile
+#  gunicorn istemişler onda kod hata veriyore gunicorn ile
 ###Traceback (most recent call last):
 ###  File "/home/vagrant/.local/bin/gunicorn", line 7, in <module>
 ###    from gunicorn.app.wsgiapp import run
