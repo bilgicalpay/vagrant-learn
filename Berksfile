76c2c2472768b96f27cb5aa16486532fc30d8716
alpay bilgiç <bilgicalpay@gmail.com>
2019-11-19 14:27:13 +0300

    fix

--- Berksfile
+++ Berksfile
@@ -2,7 +2,6 @@ source "https://supermarket.getchef.com"
 
 
 
-cookbook 'flash', '~> 2.0.0'
 #cookbook 'python'   // chef markette deprecated olmuş. Bu yüzden bootstrap.sh (ana klasorde vesatır 6) içinde kuracağım. 
 cookbook 'apt', '~> 7.2.0'
 cookbook 'python' 
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,7 @@
 sudo apt install python2.7-dev -q -y
 sudo apt install python-pip -q -y
 sudo pip install virtualenv
-
 #sudo source bin/active
-
 #sudo pip install -r requirements.txt
 #sudo pip install --upgrade pip bu başa bela 
 # düzeltmek için pip install pip==1.5.6  python 2.7 için 1.5.6
@@ -15,10 +13,8 @@ sudo apt-get build-dep python-mysqldb -y
 sudo pip install MySQL-python -y
 sudo pip install gunicorn
 sudo apt  install nginx -q -y 
+sudo pip install Flask -y
 ## amac: flask(web app)-->gunicorn(WSGI server):port-->nginx(web server):port
-
-
-
 cd /var/www
 sudo virtualenv --no-site-packages ..
 sudo mkdir /var/www/codebase
@@ -26,9 +22,6 @@ sudo cp /vagrant/* /var/www/codebase
 sudo cd /var/www/codebase
 git clone https://gist.github.com/vedit/32f37e0b8f842521fbc89e9d8e2bfaf9
 sudo cp /var/www/codebase/32f37e0b8f842521fbc89e9d8e2bfaf9/application.py ../
-
-
-
 ## asagidaki çalıştı
 python application.py
 # ama gunicorn istemişler onda kod hata veriyore gunicorn ile
