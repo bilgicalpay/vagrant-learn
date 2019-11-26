f9c29204ef452a3e58543fe35fe0b90792ea0769
Alpay Bilgiç <bilgicalpay@users.noreply.github.com>
2019-11-27 00:52:06 +0300

    Update run.sh

--- yeni/mysql/run.sh
+++ yeni/mysql/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Burada bir sorun var. şu an çalışıyor ama bir  iki kez permission hatası verdi. 
+# hata alırsa vagrant ssh ile içine girip ilk 4 komutu alıştırmak gerek. 
 sudo mkdir -p /var/lib/mysql
 sudo mkdir -p /mnt/mysql
 sudo chown -R vagrant:vagrant /var/lib/mysql
